3ecd8c107b665283d3b0faae1c12eef527251f63
Abel Quintero <abel.j.quintero@gmail.com>
2019-03-04 23:40:23 -0800

    added product page details

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,17 +1,32 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import PropTypes from 'prop-types';
+import './styles/Product.css';
 
-const Product = ({ price, inventory, title }) => (
-  <div>
-    {title} - &#36;{price}{inventory ? ` x ${inventory}` : null}
+const Product = ({ price, inventory, title, image, quantity }) => (
+  <div className = "cart-container">
+    <div className = "image-container">
+      <img id = "img" src = {image} alt = "image"/>
+    </div>
+
+    <div className = "product-details">
+      <h4>{title}</h4>
+      <p id = "price">&#36;{price}</p>
+      <p id = "remove"> Remove </p>
+      <div className = "buttons">
+        <button id = "minus"> - </button>
+        { quantity }
+        <button id = "plus"> + </button>
+      </div>
+    </div>
   </div>
 );
 
 Product.propTypes = {
   price: PropTypes.number,
   inventory: PropTypes.number,
-  title: PropTypes.string
+  title: PropTypes.string,
+  image: PropTypes.string
 };
 
 export default Product;
